673df776b40ed6e373ba83f959aac08a9828a297
Nick <deaneygaming@gmail.com>
2024-01-12 13:17:21 +0200

    Added adaptive

--- js/main.js
+++ js/main.js
@@ -13,16 +13,35 @@ function addTask() {
 		span.innerHTML = '\u00d7'
 		li.appendChild(span)
 	}
+	saveData()
 }
+
+inputBox.addEventListener('keyup', function (event) {
+	if (event.key === 'Enter') {
+		addTask()
+	}
+})
 
 listContainer.addEventListener(
 	'click',
 	function (e) {
 		if (e.target.tagName === 'LI') {
 			e.target.classList.toggle('checked')
+			saveData()
 		} else if (e.target.tagName === 'SPAN') {
 			e.target.parentElement.remove()
+			saveData()
 		}
 	},
 	false
 )
+
+function saveData() {
+	localStorage.setItem('data', listContainer.innerHTML)
+}
+
+function showTask() {
+	listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showTask()
